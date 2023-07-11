5f2dc59ad22c5d32637a222616cd9c8bc6b85f78
mhayes853 <matthewha853@gmail.com>
2023-07-11 12:40:18 -0700

    Add setup scrript

---
+++ setup.sh
@@ -0,0 +1,3 @@
+rm -f secrets.ts
+echo "export const ABLY_API_KEY = \"<Insert API key from ably here>\"" > secrets.ts
+echo "Setup completed, make sure to insert your api key from ably in secrets.ts!"
